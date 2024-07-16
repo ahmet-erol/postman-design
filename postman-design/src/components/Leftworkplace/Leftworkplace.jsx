a1e7ec1fd34e78b2a789d047dc89a3fc317a0916
Ahmet Erol <erlahmt97@gmail.com>
2024-07-17 00:07:37 +0300

    adding components

---
+++ postman-design/src/components/Leftworkplace/Leftworkplace.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Leftworkplace.css'
+
+function Leftworkplace() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Leftworkplace
---
+++ postman-design/src/components/Rightworkplace/Rightworkplace.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Rightworkplace.css'
+
+function Rightworkplace() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Rightworkplace
