72bc2bb6a498dcd6f95697c4900714efe29d45c1
Ahmet Erol <erlahmt97@gmail.com>
2024-07-22 22:56:21 +0300

    leftworkplace component

--- postman-design/src/components/Leftworkplace/Leftworkplace.jsx
+++ postman-design/src/components/Leftworkplace/Leftworkplace.jsx
@@ -1,36 +1,87 @@
 import React from 'react'
 import './Leftworkplace.css'
 import { FaRegUser } from "react-icons/fa";
+import { BsCollection } from "react-icons/bs";
+import { CiBoxes } from "react-icons/ci";
+import { RiHistoryFill } from "react-icons/ri";
+
+
+
 
 
 function Leftworkplace() {
     return (
         <div className='mainleft-div'>
             <div className='my-workspace'>
-                <div className='left-user-logo'>
-                    <FaRegUser />
+
+                <div className='workspace-left'>
+
+                    <div className='left-user-logo'>
+                        <FaRegUser className='workspace-logo' />
+                        <div className='myworkspace-texting'>
+                            My Workspace
+                        </div>
+                    </div>
                 </div>
 
-                <div className='left-myworkspace'>
-                    My Workspace
+
+                <div className='workspace-right'>
+                    <div className='.left-newbutton'>
+                        <button type='button' className='left-newbutton-inside'>
+                            New
+                        </button>
+                    </div>
+                    <div className='left-importbutton'>
+                        <button type='button' className='left-importbutton-inside'>
+                            Import
+                        </button>
+                    </div>
+
                 </div>
 
-                <div>
-                    <button type='button' className='left-newbutton'>
-                        New
-                    </button>
-
-                    <button type='button' className='left-importbutton'>
-                        Import
-                    </button>
-                </div>
 
             </div>
 
 
 
             <div className='options'>
-                <div>
+                <div className='options-buttons-left acordionleft'>
+                    <button className='left-collections-button'>
+                        <BsCollection className='collections-but' />
+                        Collections
+                    </button>
+
+                    <button className='left-environments-button'>
+                        <CiBoxes className='envi-but' />
+                        Environments
+                    </button>
+
+                    <button className='left-history-button'>
+                        <RiHistoryFill className='history-but' />
+                        History
+                    </button>
+                </div>
+
+
+                <div className='options-buttons acordionmenu'>
+                    <button className='left-collections-button'>
+                        Collections
+                    </button>
+                    <button className='left-environments-button'>
+                        Environments
+                    </button>
+                    <button className='left-history-button'>
+                        History
+                    </button>
+                    <button className='left-collections-button'>
+                        Collections
+                    </button>
+                    <button className='left-environments-button'>
+                        Environments
+                    </button>
+                    <button className='left-history-button'>
+                        History
+                    </button>
                     <button className='left-collections-button'>
                         Collections
                     </button>
@@ -43,13 +94,8 @@ function Leftworkplace() {
                 </div>
             </div>
 
+        </div >
 
-
-
-            <div className='services'>
-                3
-            </div>
-        </div>
 
     )
 }
