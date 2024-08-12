177210f5345f6242137d0672210e66d716eecc91
ahmet-erol <erlahmt97@gmail.com>
2024-08-13 00:47:35 +0300

    creating 3th tab and editing other tabs

--- postman-design/src/components/Tabs/Tabs1/Tabs1.jsx
+++ postman-design/src/components/Tabs/Tabs1/Tabs1.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from "react";
 import "./Tabs1.css";
+import { SiPostman } from "react-icons/si";
 
 function Tabs1() {
   return (
     <div className="main-tab-div">
-      <div className="header-text-tab1">Query Params</div>
-      <div>
-        <table class="custom-table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Key</th>
-              <th>Value</th>
-              <th>Description</th>
-              <th>Bulk Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td>
-                <input type="text" placeholder="Key" />
-              </td>
-              <td>
-                <input type="text" placeholder="Value" />
-              </td>
-              <td class="description" colspan="2">
-                <input type="text" placeholder="Description" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="up-div">
+        <div className="header-text-tab1">Query Params</div>
+        <div>
+          <table class="custom-table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Key</th>
+                <th>Value</th>
+                <th>Description</th>
+                <th>Bulk Edit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="text" placeholder="Key" />
+                </td>
+                <td>
+                  <input type="text" placeholder="Value" />
+                </td>
+                <td class="description" colspan="2">
+                  <input type="text" placeholder="Description" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="down-div">
+        Response
+        <div className="response-icon">
+          <SiPostman />
+          <div className="response-icontext">Click Send to get a response</div>
+        </div>
       </div>
     </div>
   );
--- postman-design/src/components/Tabs/Tabs3/Tabs3.jsx
+++ postman-design/src/components/Tabs/Tabs3/Tabs3.jsx
@@ -1,53 +1,64 @@
 import React, { useState } from "react";
 import "./Tabs3.css";
+import { SiPostman } from "react-icons/si";
 
 function Tabs3() {
   return (
     <div className="main-tab3-div">
-      <div className="header-text-tab3">Headers</div>
+      <div className="up-div-tab3">
+        <div className="header-text-tab3">Headers</div>
 
-      <div>
-        <table class="custom-table-tab3">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Key</th>
-              <th>Value</th>
-              <th>Description</th>
-              <th>Bulk Edit</th>
-              <th>Presets</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td>
-                <input type="text" defaultValue="Authorization" />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  defaultValue="BU0FSIERFVEFZIn1dfSx7IklkIjo"
-                />
-              </td>
-              <td class="description" colspan="3">
-                <input type="text" placeholder="Description" />
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>
-                <input type="text" defaultValue="CompanyIdentifier" />
-              </td>
-              <td>
-                <input type="text" defaultValue="85ee5631-ab92" />
-              </td>
-              <td class="description" colspan="3">
-                <input type="text" placeholder="Description" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <table class="custom-table-tab3">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Key</th>
+                <th>Value</th>
+                <th>Description</th>
+                <th>Bulk Edit</th>
+                <th>Presets</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="text" defaultValue="Authorization" />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    defaultValue="BU0FSIERFVEFZIn1dfSx7IklkIjo"
+                  />
+                </td>
+                <td class="description" colspan="3">
+                  <input type="text" placeholder="Description" />
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="text" defaultValue="CompanyIdentifier" />
+                </td>
+                <td>
+                  <input type="text" defaultValue="85ee5631-ab92" />
+                </td>
+                <td class="description" colspan="3">
+                  <input type="text" placeholder="Description" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="down-div-tab3">
+        Response
+        <div className="response-icon">
+          <SiPostman />
+          <div className="response-icontext">Click Send to get a response</div>
+        </div>
       </div>
     </div>
   );
