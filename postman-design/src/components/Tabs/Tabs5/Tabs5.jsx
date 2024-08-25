869ca4c9e375e2f1d573e670bb5c640887cb274d
ahmet-erol <erlahmt97@gmail.com>
2024-08-25 17:21:11 +0300

    scrollbar for tabs5 and

--- postman-design/src/components/Tabs/Tabs5/Tabs5.jsx
+++ postman-design/src/components/Tabs/Tabs5/Tabs5.jsx
@@ -12,17 +12,38 @@ function Tabs5() {
       </div>
 
       <div className="tabs5-middle">
-        <textarea name="text-area" id="textarea" className="middle-textarea">
-          1 Use JavaScript to configure this request dynamically.
-        </textarea>
+        <textarea
+          name="text-area"
+          id="textarea"
+          placeholder="Use JavaScript to configure this request dynamically."
+          className="middle-textarea"
+        ></textarea>
       </div>
 
       <div className="tabs5-right">
-        <div className="tabs5-right-headertext">Packages</div>
-        <div className="tabs5-right-openpackages">
-          <div>Open package library</div>
-          <div>
-            <BsArrowRight />
+        <div className="tabs5-right-top">
+          <div className="tabs5-right-headertext">Packages</div>
+          <div className="tabs5-right-openpackages">
+            <div>Open package library</div>
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+        <div className="tabs5-right-down">
+          <div className="tabs5-right-down-slidercontent">
+            <p className="snippets">Snippets</p>
+            <p>Get an environment variable</p>
+            <p>Get a global variable</p>
+            <p>Get a variable</p>
+            <p>Get a collection variable</p>
+            <p>Set an environment variable</p>
+            <p>Set a global variable</p>
+            <p>Set a collection variable</p>
+            <p>Clear an environment variable</p>
+            <p>Clear a global variable</p>
+            <p>Clear a colletion variable</p>
+            <p>Send a request</p>
           </div>
         </div>
       </div>
