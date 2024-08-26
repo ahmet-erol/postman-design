08262b491ff9fff03ec4dcd8e3863490df773d46
ahmet-erol <erlahmt97@gmail.com>
2024-08-26 23:28:24 +0300

    the end of the html and css structure of tabs5

--- postman-design/src/components/Tabs/Tabs5/Tabs5.jsx
+++ postman-design/src/components/Tabs/Tabs5/Tabs5.jsx
@@ -6,48 +6,51 @@ import { SiPostman } from "react-icons/si";
 function Tabs5() {
   return (
     <div className="tabs5-main-div">
-      <div className="tabs5-left">
-        <button className="tabs5-left-button">Pre-request</button>
+      <div className="tabs5-top">
+        <div className="tabs5-left">
+          <button className="tabs5-left-button">Pre-request</button>
 
-        <button className="tabs5-left-button">Post-response</button>
-      </div>
+          <button className="tabs5-left-button">Post-response</button>
+        </div>
 
-      <div className="tabs5-middle">
-        <textarea
-          name="text-area"
-          id="textarea"
-          placeholder="Use JavaScript to configure this request dynamically."
-          className="middle-textarea"
-        ></textarea>
-      </div>
+        <div className="tabs5-middle">
+          <textarea
+            name="text-area"
+            id="textarea"
+            placeholder="Use JavaScript to configure this request dynamically."
+            className="middle-textarea"
+          ></textarea>
+        </div>
 
-      <div className="tabs5-right">
-        <div className="tabs5-right-top">
-          <div className="tabs5-right-headertext">Packages</div>
-          <div className="tabs5-right-openpackages">
-            <div>Open package library</div>
-            <div>
-              <BsArrowRight />
+        <div className="tabs5-right">
+          <div className="tabs5-right-top">
+            <div className="tabs5-right-headertext">Packages</div>
+            <div className="tabs5-right-openpackages">
+              <div>Open package library</div>
+              <div>
+                <BsArrowRight />
+              </div>
+            </div>
+          </div>
+          <div className="tabs5-right-down">
+            <div className="tabs5-right-down-slidercontent">
+              <p className="snippets">Snippets</p>
+              <p>Get an environment variable</p>
+              <p>Get a global variable</p>
+              <p>Get a variable</p>
+              <p>Get a collection variable</p>
+              <p>Set an environment variable</p>
+              <p>Set a global variable</p>
+              <p>Set a collection variable</p>
+              <p>Clear an environment variable</p>
+              <p>Clear a global variable</p>
+              <p>Clear a colletion variable</p>
+              <p>Send a request</p>
             </div>
           </div>
         </div>
-        <div className="tabs5-right-down">
-          <div className="tabs5-right-down-slidercontent">
-            <p className="snippets">Snippets</p>
-            <p>Get an environment variable</p>
-            <p>Get a global variable</p>
-            <p>Get a variable</p>
-            <p>Get a collection variable</p>
-            <p>Set an environment variable</p>
-            <p>Set a global variable</p>
-            <p>Set a collection variable</p>
-            <p>Clear an environment variable</p>
-            <p>Clear a global variable</p>
-            <p>Clear a colletion variable</p>
-            <p>Send a request</p>
-          </div>
-        </div>
       </div>
+
       <div className="tabs5-bottom">
         Response
         <div className="response-icon-tab4">
