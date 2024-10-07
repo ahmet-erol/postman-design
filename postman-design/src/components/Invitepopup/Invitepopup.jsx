a39d964886f7d2a54d7699d20a38486b8ed80be3
ahmet-erol <erlahmt97@gmail.com>
2024-10-08 00:03:33 +0300

    invite popup

--- postman-design/src/components/Invitepopup/Invitepopup.jsx
+++ postman-design/src/components/Invitepopup/Invitepopup.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import "./Invitepopup.css";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 function Invitepopup() {
   return (
     <div className="invite-main">
-      <div className="invite-top">bu bir başlık</div>
-      <div className="invite-mid">bu bir mid</div>
-      <div className="invite-bottom">bu bir alt satır</div>
+      <div className="invite-content">
+        <div className="invite-top">Invite people to this workspace</div>
+        <div className="invite-mid">
+          <IoInformationCircleOutline size={18} />
+          Inviting people will change this into a team workspace.
+        </div>
+        <div className="invite-bottom">
+          <div className="invite-bottom-subdiv">
+            <div className="questions">Name, email, or group name</div>
+            <div className="add-files">Add from file</div>
+          </div>
+          <textarea name="" id="" className="invite-textarea"></textarea>
+          <button className="send-invite-button">Send Invite</button>
+        </div>
+      </div>
     </div>
   );
 }
